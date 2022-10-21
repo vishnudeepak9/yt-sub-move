a003c4dd23146e18848e0963ae573f157fb23eb3
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-10-21 07:41:50 +0530

    first commit

--- youtube.py
+++ youtube.py
@@ -8,5 +8,12 @@ with open("subscriptions.csv",'r') as file:
         c+=1
             #print(i[1])
         webbrowser.open_new(i[1])
+        """
         if c%10==0:
             time.sleep(60)
+        #this code is used to execute every 10 subscriptions at a time
+        """
+
+        #we can modify this to one subscription and wait till 10 seconds and then continue
+        time.sleep(10)
+        
